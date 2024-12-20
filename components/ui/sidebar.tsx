1a2e81b273b9e7ad6039e8eabbe78654af214f20
ug <subrahmanyaug@gmail.com>
2024-12-20 13:01:28 +0530

    responsive width issue fixed

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -21,8 +21,8 @@ import {
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-const SIDEBAR_WIDTH = "6rem"
-const SIDEBAR_WIDTH_MOBILE = "8rem"
+const SIDEBAR_WIDTH = "4rem"
+const SIDEBAR_WIDTH_MOBILE = "6rem"
 const SIDEBAR_WIDTH_ICON = "3rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
