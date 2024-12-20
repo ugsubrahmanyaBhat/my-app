93df1745aaa9927d06d4bfb06b78c4e4b41c8b54
hegade <subramanyashegade@gmail.com>
2024-12-20 14:39:40 +0530

    feat: add api for swaps

---
+++ app/api/route.ts
@@ -0,0 +1,11 @@
+// app/api/hello/route.ts
+import { NextResponse } from 'next/server'
+ 
+export async function GET() {
+  return NextResponse.json({ message: 'Hello World!' })
+}
+
+export async function POST(request: Request) {
+  const data = await request.json()
+  return NextResponse.json({ data })
+}
